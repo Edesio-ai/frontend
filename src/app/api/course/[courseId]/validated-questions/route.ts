8c6e7a8edb4512f2b9ef9267f984bd45392c6e47
Nicolas Marin <nicolasmarin@Host-003.lan>
2026-04-28 14:43:46 +0200

    hotfix(teacher): return code on validated questions endpoint

--- src/app/api/course/[courseId]/validated-questions/route.ts
+++ src/app/api/course/[courseId]/validated-questions/route.ts
@@ -16,7 +16,7 @@ export async function GET(request: NextRequest, { params }: RouteContexte) {
 
   if(!response.ok) {
     const error = await response.json();
-    return NextResponse.json({ error: error.message }, { status: 500 });
+    return NextResponse.json({ error: error.message, code: error?.code }, { status: 500 });
   }
 
   const data = await response.json();
