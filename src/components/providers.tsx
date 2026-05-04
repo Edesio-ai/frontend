de8557342e6fd62aec4ba43d5af869a4664ca9d0
Nicolas Marin <nicolasmarin@Host-003.lan>
2026-05-04 19:19:54 +0200

    hotfix(toaster): fix toaster display

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -3,7 +3,13 @@
 import type { ReactNode } from "react";
 
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "./ui/toaster";
 
 export function Providers({ children }: { children: ReactNode }) {
-  return <TooltipProvider>{children}</TooltipProvider>;
+  return (
+    <TooltipProvider>
+      {children}
+      <Toaster />
+    </TooltipProvider>
+  );
 }
