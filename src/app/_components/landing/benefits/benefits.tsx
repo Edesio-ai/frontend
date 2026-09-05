4fabc6664336fce98a933e87875cca63908dd103
Nicolas Marin <nicolasmarin@MacBook-Air.lan>
2026-09-05 20:28:56 +0200

    feat(#213): create stats banner for landing page

--- src/app/_components/landing/benefits/benefits.tsx
+++ src/app/_components/landing/benefits/benefits.tsx
@@ -2,12 +2,18 @@
 import { useFeatureFlag } from "@/contexts/feature-flags-context";
 import { BenefitsLegacy } from "./benefits-legacy";
 import { BenefitsNew } from "./benefits-new";
+import { StatsBanner } from "../stats/stats-banner";
 
 export function Benefits() {
   const isNewHomepage = useFeatureFlag("HomepageNewDesign");
 
   if (isNewHomepage) {
-    return <BenefitsNew />;
+    return (
+      <>
+        <BenefitsNew />
+        <StatsBanner />
+      </>
+    );
   }
 
   return <BenefitsLegacy />;
